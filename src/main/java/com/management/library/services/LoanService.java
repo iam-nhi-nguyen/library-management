30e45129d1784517330a5f7a2a70c3a605a79a50
nguyennganhi <49206582+nguyennganhi@users.noreply.github.com>
2021-06-25 13:07:57 +0700

    Complete loan tab with id search functions

--- src/main/java/com/management/library/services/LoanService.java
+++ src/main/java/com/management/library/services/LoanService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.util.*;
 
 @Service
 @Transactional
@@ -53,5 +53,11 @@ public class LoanService extends CrudService<Loan, Long> {
             loan.setBook(book);
         }
         return result;
+    }
+
+    @Override
+    protected void beforeCreate(Loan entity) {
+        super.beforeCreate(entity);
+        entity.setUpdated(null);
     }
 }
